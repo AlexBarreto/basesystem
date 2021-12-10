49d83691a76cfb6545522f8dead1917d611a811d
AlexBarreto <alexbarreto2020@hotmail.com>
2021-12-10 14:32:06 -0300

    primeiro commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '0084335052d67e3b1ad8a640a7d429e0a33cfc22',
+        'name' => 'barretodev/basesystem',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'barretodev/basesystem' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '0084335052d67e3b1ad8a640a7d429e0a33cfc22',
+            'dev_requirement' => false,
+        ),
+    ),
+);
