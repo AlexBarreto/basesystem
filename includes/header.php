313ca3cd226a5e184c4ea5c5e8453763bc864150
AlexBarreto <alexbarreto2020@hotmail.com>
2021-12-10 14:31:14 -0300

    primary commit

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      content="width=device-width, initial-scale=1, maximum-scale=1"
+      name="viewport"
+    />
+    <title>Base system</title>
+    <link href="/bootstrap/css/bootstrap.css.map" rel="stylesheet" />
+    <script src="/bootstrap/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Contato</li>
+        </ul>
+      </nav>
+    </header>
+
+  <body>
